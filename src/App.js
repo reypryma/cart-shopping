029c38c9ee78d8fe8f8c8971b41967afc768647a
Re <refanopradana@gmail.com>
2020-07-12 20:42:43 +0700

    Product Lists:Create Sidebar, add data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React from 'react';
+import data from "./data.json";
 
+class App extends React.Component{
 
-function App() {
-  return (
-      <div className="grid-container">
-        <header className="">
-          <header>
-            <a href="/">React Shopping Cart</a>
-          </header>
-        </header>
-        <main>
-          Product List
-        </main>
-        <footer>
-          All right is reserved
-        </footer>
-      </div>
-  );
+    constructor() {
+        super();
+        this.state = {
+            products:data.products,
+            size:"",
+            sort:"",
+        }
+    }
+
+    render() {
+      return (
+          <div className="grid-container">
+              <header className="">
+                  <header>
+                      <a href="/">React Shopping Cart</a>
+                  </header>
+              </header>
+              <main>
+                  <div className="content">
+                      <div className="main">
+                        Products
+                      </div>
+                      <div className="sidebar">
+                          Cart Items
+                      </div>
+                  </div>
+              </main>
+              <footer>
+                  All right is reserved
+              </footer>
+          </div>
+      );
+  }
 }
 
 export default App;
