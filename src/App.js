25ee4a302d766bc5d972513cc2298b8eae61c1cf
Re <refanopradana@gmail.com>
2020-07-13 17:41:26 +0700

    Filter Component 02 : Get Logic to Filter.js sort product latest, lowest, highest

--- src/App.js
+++ src/App.js
@@ -13,12 +13,41 @@ class App extends React.Component {
         }
     }
 
-    sortProducts(event){
+    sortProducts = (event) =>{
+        const sort = event.target.value;
         console.log(event.target.value);
-    }
-    filterProducts(event){
+        this.setState(
+            (state) => ({
+                sort: sort,
+                products: this.state.products.slice()
+                    .sort(
+                    (a, b) =>
+                        sort === "lowest" ? a.price > b.price ? 1 : -1
+                            :
+                        sort === "highest" ? a.price < b.price ? 1 : -1
+                             :
+                        a._id > b._id ? 1 : -1
+                )
+            }))
+    };
+    //change to method function by using arrow so can access state
+    filterProducts=(event)=>{
         console.log(event.target.value);
-    }
+        if (event.target.value === ""){
+            this.setState({
+                size: event.target.value, product: data.products
+            })
+        }else{
+            this.setState({
+                size: event.target.value,
+                products: data.products.filter(
+                    (product) => product.availableSizes.indexOf(
+                        event.target.value
+                    )>=0
+                ),
+            })
+        }
+    };
 
     render() {
         return (
