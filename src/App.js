58cdbc9d3a84a7227cf226d3346f025716912213
Re <refanopradana@gmail.com>
2020-07-13 21:40:52 +0700

    Cart Component 02 : Method addToCart work and view in sidebar + css design

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import data from "./data.json";
 import Products from "./components/Products";
 import Filter from "./components/Filter";
+import Cart from "./components/Cart";
 
 class App extends React.Component {
     constructor() {
@@ -14,7 +15,7 @@ class App extends React.Component {
         }
     }
 
-    sortProducts = (event) =>{
+    sortProducts = (event) => {
         const sort = event.target.value;
         console.log(event.target.value);
         this.setState(
@@ -22,48 +23,47 @@ class App extends React.Component {
                 sort: sort,
                 products: this.state.products.slice()
                     .sort(
-                    (a, b) =>
-                        sort === "lowest" ? a.price > b.price ? 1 : -1
-                            :
-                        sort === "highest" ? a.price < b.price ? 1 : -1
-                             :
-                        a._id > b._id ? 1 : -1
-                )
+                        (a, b) =>
+                            sort === "lowest" ? a.price > b.price ? 1 : -1
+                                :
+                                sort === "highest" ? a.price < b.price ? 1 : -1
+                                    :
+                                    a._id > b._id ? 1 : -1
+                    )
             }))
     };
     //change to method function by using arrow so can access state
-    filterProducts=(event)=>{
+    filterProducts = (event) => {
         console.log(event.target.value);
-        if (event.target.value === ""){
+        if (event.target.value === "") {
             this.setState({
                 size: event.target.value, products: data.products
             })
-        }else{
+        } else {
             this.setState({
                 size: event.target.value,
                 products: data.products.filter(
                     (product) => product.availableSizes.indexOf(
                         event.target.value
-                    )>=0
+                    ) >= 0
                 ),
             })
         }
     };
 
-    addToCart=(product)=>{
+    addToCart = (product) => {
         const cartItems = this.state.cartItems.slice();
         let alreadyInCart = false;
-        cartItems.forEach(
-            item=>{
-                if (item._id === product._id){
-                    item.count++;
-                    alreadyInCart = true;
-                }
+        cartItems.forEach((item) => {
+            if (item._id === product._id) {
+                item.count++;
+                alreadyInCart = true;
             }
-        );
-        if (!alreadyInCart){
-            cartItems.push({...product, count: 1})
+        });
+        if (!alreadyInCart) {
+            cartItems.push({...product, count: 1});
         }
+        this.setState({cartItems});
     };
 
     render() {
@@ -80,17 +80,16 @@ class App extends React.Component {
                             <Filter count={this.state.products.length}
                                     size={this.state.size}
                                     sort={this.state.sort}
-                            /*        Handling changing size and sort  */
-                                filterProducts={this.filterProducts}
+                                /*        Handling changing size and sort  */
+                                    filterProducts={this.filterProducts}
                                     sortProducts={this.sortProducts}
-                            >
-                            </Filter>
-                            <Products products={this.state.products}>
-
-                            </Products>
+                            />
+                            <Products products={this.state.products}
+                                      addToCart={this.addToCart}
+                            />
                         </div>
                         <div className="sidebar">
-                            Cart Items
+                            <Cart cartItems={this.state.cartItems}/>
                         </div>
                     </div>
                 </main>
