5b1351d949fc78166e0d09346a3f75d21c039359
Re <refanopradana@gmail.com>
2020-07-17 20:48:16 +0700

    05 Use Redux Order to cart component

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,22 +1,21 @@
 import {CLEAR_CART, CLEAR_ORDER, CREATE_ORDER} from "../utils/types";
 
-export const createOrder = (order) => (dispatch) =>{
-    fetch("/api/orders",{
-      method: "Post",
-      headers: {
-          "Content-Type": "application/json",
-      },
-      body:JSON.stringify(order),
+export const createOrder = (order) => (dispatch) => {
+    fetch("/api/orders", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(order),
     })
-        .then((res) =>
-            (res.json())
-        ).then((data) => {
-            dispatch({type: CREATE_ORDER, payload: data});
+        .then((res) => res.json())
+        .then((data) => {
+            dispatch({ type: CREATE_ORDER, payload: data });
             localStorage.clear("cartItems");
-            dispatch({type: CLEAR_CART})
-    })
+            dispatch({ type: CLEAR_CART });
+        });
+};
+export const clearOrder = () => (dispatch) => {
+    dispatch({ type: CLEAR_ORDER });
 };
 
-export const clearOrder = () => (dispatch) =>{
-    dispatch({type: CLEAR_ORDER});
-};
