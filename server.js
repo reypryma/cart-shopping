9fc67270c983d9987b5be5509ed0a33488f38654
Re <refanopradana@gmail.com>
2020-07-17 13:10:40 +0700

    01 Create Api for order in server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
 const Product = mongoose.model(
     "products",
     new mongoose.Schema({
-        _id: { type: String, default: shortid.generate },
+        _id: {type: String, default: shortid.generate},
         title: String,
         description: String,
         image: String,
@@ -46,10 +46,51 @@ app.delete("/api/products/:id", async (req, res) => {
     res.send(deletedProduct);
 });
 
+//Order api
+const Order = mongoose.model(
+    "order",
+    new mongoose.Schema(
+        {
+            _id: {
+                type: String,
+                default: shortid.generate,
+            },
+            email: String,
+            name: String,
+            address: String,
+            total: Number,
+            cartItems: [
+                {
+                    _id: String,
+                    title: String,
+                    price: Number,
+                    count: Number,
+                },
+            ],
+        },
+        {
+            timestamps: true,
+        }
+    )
+);
+
+app.post("/api/orders", async (req, res) => {
+    if (
+        !req.body.name ||
+        !req.body.email ||
+        !req.body.address ||
+        !req.body.total ||
+        !req.body.cartItems
+    ) {
+        return res.send({message: "Data is required."});
+    }
+    const order = await Order(req.body).save();
+    res.send(order);
+});
 
 //launch server
 const port = process.env.PORT || 5000;
 
-app.listen(port, "localhost", ()=>{
+app.listen(port, "localhost", () => {
     console.log('App listening on http://localhost:' + port);
 });
